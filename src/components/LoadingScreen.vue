85c1c266f4c5335ba8e38e23c33fa265f0ecbab2
Lester John Rodney Intia <i.lesterjohnrodney@gmail.com>
2024-04-20 17:08:16 +0800

    save

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -1,12 +1,20 @@
 <template>
-  <div v-if="loading" class="loader">
-    <!-- Your loader content here -->
-    Loading...
+  <!-- Your loader content here -->
+  <div v-if="loading" class="loading-container">
+    <p>PARAMOUNT</p>
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
+
   <router-view v-else></router-view>
 </template>
 
 <script>
+import { RouterView } from "vue-router";
 export default {
   data() {
     return {
@@ -23,7 +31,78 @@ export default {
 </script>
 
 <style>
-.loader {
-  /* Loader styles */
+.loading-container {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.loading-container p {
+  color: #04a533;
+  font-size: 2vh;
+  font-weight: 400;
+}
+
+.lds-ellipsis,
+.lds-ellipsis div {
+  box-sizing: border-box;
+}
+.lds-ellipsis {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 33.33333px;
+  width: 13.33333px;
+  height: 13.33333px;
+  border-radius: 50%;
+  background: #04a533;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 8px;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 8px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 32px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 56px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(24px, 0);
+  }
 }
 </style>
